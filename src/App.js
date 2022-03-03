2664b09bc86bc9f77d42d035d98829674e78ffb2
Zain Imtiaz <90681723+zenimtiazz@users.noreply.github.com>
2022-03-03 12:23:22 +0100

    back button and header worked finally

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import Back from './back/Back';
 import Topbar from './topbar/Topbar';
 
 function App() {
   return (
     <div className="App">
       <Topbar />
+      <Back  />
     </div>
   );
 }
